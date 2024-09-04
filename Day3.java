99fb09b9c1e28241c2f4354e67e0ed3872788d4a
Jodim275 <myacoun447@gmail.com>
2024-09-04 16:09:11 +0800

    Day3.java
    Systen.out.println

---
+++ Day3.java
@@ -0,0 +1,8 @@
+package Coding100Hari;
+public class Day2 {
+	public static void main(String[] args) {
+		System.out.println("Nama : Jodim Robertus");
+		System.out.println("NIM : D0224312");
+		System.out.println("Alamat : Tande");
+	}
+}
