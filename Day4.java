95cce378b2d9e8c555c46d14cfb6831723778682
Jodim275 <myacoun447@gmail.com>
2024-09-05 13:15:24 +0800

    Day4.java
    System.out.print dan System.out.printf

---
+++ Day4.java
@@ -0,0 +1,12 @@
+package Coding100Hari;
+
+public class Day4 {
+	public static void main(String[] args) {
+		System.out.print("Data 3 nilai tertinggi : ");
+		System.out.print("Ling=86, ");
+		System.out.print("Fanny=90, ");
+		System.out.print("Joy=80. ");
+		System.out.printf("Siswa dengan nilai tertinggi adalah %s dengan nilai %d.", "Fanny",90);
+		
+	}
+}
