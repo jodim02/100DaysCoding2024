cdf0d37f93ab368743b829c78d720be3249f3884
Jodim275 <myacoun447@gmail.com>
2024-09-06 18:22:57 +0800

    Day5.java
    Membuat komentar

---
+++ Day5.java
@@ -0,0 +1,15 @@
+package Coding100Hari;
+public class Day5 {
+	public static void main(String[] args) {
+		//Biodata
+		System.out.println("Nama   :"+"Jodim");
+		System.out.println("NIM    :"+"D0224312");
+		System.out.println("Prodi  :"+"Informatika");
+		/*
+		Isi bioadata:
+		1.Nama
+		2.NIM
+		3.Program Studi
+		*/
+	}
+}
