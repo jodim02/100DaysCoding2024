d6b11e3594f7d549b898b18481326ceb7ff04aae
Jodim275 <myacoun447@gmail.com>
2024-09-08 15:05:55 +0800

    Day7.java
    Variabel : Inisialisasi

---
+++ Day7.java
@@ -0,0 +1,14 @@
+package Coding100Hari;
+public class Main {
+	public static void main(String[] args) {
+    //Inisialisasi Variabel 
+		String nama = "Jodim ";
+		int umur = 19 ;
+		char JenisKelamin = 'L';
+
+    //Menampilkan Nilai Variabel
+		System.out.println("Nama             :" +nama);
+		System.out.println("Umur             :" +umur);
+		System.out.println("Jenis Kelamin    :" +JenisKelamin);
+	}
+}
