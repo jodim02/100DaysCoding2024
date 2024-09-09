01e0ada6c098a0a8121c4635054695a1ad904e60
Jodim275 <myacoun447@gmail.com>
2024-09-09 19:36:14 +0800

    Day8.java
    Variabel : Update

---
+++ Day8.java
@@ -0,0 +1,23 @@
+package Coding100Hari;
+public class Day8 {
+	public static void main(String[] args) {
+		//Nilai awal variabel
+		String nama = "Jodim Robertus ";
+		int umur = 18 ;
+		String alamat = "Kec. Banggai Timur";
+		
+		System.out.println("Nama     :" +nama);
+		System.out.println("Umur     :" +umur);
+		System.out.println("Alamat   :" +alamat);
+		
+		System.out.println("\n");
+		//Nilai variabel setelah update
+		nama = "Jodim";
+		umur = 19;
+		alamat = "Tande";
+		
+		System.out.println("Nama    :"+nama);
+		System.out.println("Umur    :" +umur);
+		System.out.println("Alamat :" +alamat);	
+	}
+      }
