543d33ba58f6a10f8e3b34ca33cbfe5967c6797c
Jodim275 <myacoun447@gmail.com>
2024-09-10 19:53:34 +0800

    Day9.java

--- Day9.java
+++ Day9.java
@@ -1,14 +1,16 @@
 package Coding100Hari;
 public class Day9{
-  //Membuat Konstanta variabel 
+  //Membuat Konstanta variabel
+  //Konstanta Tipe File
 
-  //Kontanta Tipe File
+  //Deklarasi konstanta 
     public static final String PDF = ".pdf";
     public static final String DOC = ".doc";
     public static final String PPT = ".ppt";
     public static final String TXT = ".txt";
 
     public static void main(String[] args) {
+      //Menampilkan nilai
         System.out.println("Contoh Tipe file: " + PDF +","+ DOC+"," + PPT+"," + TXT);
     }
 }
