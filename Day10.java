efeb695ad7b2d7f4b9008c7f336c6919b01a6ed0
Jodim275 <myacoun447@gmail.com>
2024-09-12 12:26:54 +0800

    Day10.java
    Tipe data: byte

--- Day10.java
+++ Day10.java
@@ -2,8 +2,8 @@ package Coding100Hari;
 public class Day10 {
 	public static void main(String[] args) {
     //Tipe data byte
-		byte NilaiMax = 100;
-		byte NilaiMin = 60;
+		byte NilaiMax = 127;
+		byte NilaiMin = -128;
 		
 		//Menampilakan nilai
 		System.out.println("Nilai Maksimum adalah "+NilaiMax);
