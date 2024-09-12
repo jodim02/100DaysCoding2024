52f0c23f0d5c900702c87036cbd1aea830b5fcf9
Jodim275 <myacoun447@gmail.com>
2024-09-12 12:50:06 +0800

    Day11.java
    Tipe data: short

---
+++ Day11.java
@@ -0,0 +1,9 @@
+package Coding100Hari;
+public class Day11 {
+	public static void main(String[] args) {
+		short max = 32767;
+		short min = - 32768;
+		
+		System.out.println("Nilai maksimal adalah "+ max +", "+"dan nilai minimal adalah "+min);
+	}
+}
