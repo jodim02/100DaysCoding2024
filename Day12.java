fb11b938152a14d08ee2bd9096c311fca61d85eb
Jodim275 <myacoun447@gmail.com>
2024-09-13 14:35:48 +0800

    Day12.java
    Tipe data: int

---
+++ Day12.java
@@ -0,0 +1,11 @@
+package Coding100Hari;
+public class Main {
+	public static void main(String[] args) {
+		//Tipe data integer
+		int max = 35;
+		int min = 25;
+		
+		System.out.println("Batas usia maksimal "+max+" tahun");
+		System.out.println("Batas usia minimal "+min+" tahun");
+	}
+}
