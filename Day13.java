075524ea2cd828d201605ee15423d6d9a5182cb9
Jodim275 <myacoun447@gmail.com>
2024-09-14 16:38:29 +0800

    Day13.java
    Evaluasi 1

---
+++ Day13.java
@@ -0,0 +1,21 @@
+package evaluasi;
+public class evaluasi {
+    public static void main(String[] args) {
+        
+        //Soal 1
+        byte nilai1 = 24;
+        short nilai2 = 35;
+        int nilai3 = 40;
+        
+        System.out.println("Nilai 1 = "+nilai1);
+        System.out.println("Nilai 2 = "+nilai2);
+        System.out.println("Nilai 3 = "+nilai3);
+        
+        System.out.println(" ");
+        
+        //Soal 2
+        String h = "day";
+        int a = 13;
+        System.out.println(h+" "+a);
+    }
+}
