dbb75b2345c87f00931380f2e22c7a1be111a1ce
Jodim275 <myacoun447@gmail.com>
2024-09-15 20:07:29 +0800

    Day14.java
    Tipe data : long

---
+++ Day14.java
@@ -0,0 +1,13 @@
+package Coding100Hari;
+public class Day14 {
+	public static void main(String[] args) {
+		
+		//Tipe data long
+		
+		long rek1 = 800000L;
+		long rek2 = 200000L;
+		
+		System.out.println("Total pengeluaran : "+"Rp"+rek1);
+		System.out.println("Sisa saldo rekening : "+"Rp"+rek2);
+	}
+}
