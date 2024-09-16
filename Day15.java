0fc6cd979f1863eeb7a6b6f64b1cd4591d0aeeb7
Jodim275 <myacoun447@gmail.com>
2024-09-16 22:16:12 +0800

    Day15.java
    Tipe data : double

---
+++ Day15.java
@@ -0,0 +1,10 @@
+package Coding100Hari;
+public class Day15 {
+	public static void main(String[] args) {
+		double pelari1 = 3.6;
+		double pelari2 = 3.4;
+		
+		System.out.println("Jarak yang ditempuh oleh pelari 1 adalah "+pelari1+" km");
+		System.out.println("Jarak yang ditempuh oleh pelari 2 adalah "+pelari2+" km");
+	}
+}
