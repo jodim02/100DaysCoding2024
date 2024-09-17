6037b9d13113b53eef117472e0630d9109ee24c5
Jodim275 <myacoun447@gmail.com>
2024-09-17 21:09:33 +0800

    Day16.java
    Tipe data : float

---
+++ Day16.java
@@ -0,0 +1,10 @@
+package Coding100Hari;
+public class Day16 {
+    public static void main(String[] args) {
+        float suhu1 = 32.5f;
+        float suhu2 = 24.7f;
+        
+        System.out.println("Suhu siang hari: " + suhu1 + " derajat Celcius");
+        System.out.println("Suhu malam hari: " + suhu2 + " derajat Celcius");
+    }
+}
