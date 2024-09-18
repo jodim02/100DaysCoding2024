883be34de1f1f28a09b97c5d26d9d4f431832891
Jodim275 <myacoun447@gmail.com>
2024-09-18 20:42:04 +0800

    Day17.java
    Tipe data : char

---
+++ Day17.java
@@ -0,0 +1,11 @@
+package Coding100Hari;
+
+public class Day17 {
+    
+    public static void main(String[] args) {
+        
+        char Inisial = 'J';
+        System.out.println("Inisial :" + Inisial);
+    }
+    
+}
