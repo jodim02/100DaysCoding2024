b38dfdf723c36fe1cc117e35a18ec67bc98689fb
Jodim275 <myacoun447@gmail.com>
2024-09-19 21:33:57 +0800

    Day18.java
    Tipe data : boolean

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package Coding100Hari;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        boolean bool1 = true;
+        boolean bool2 = false;
+        
+        System.out.println("2 < 7 =" + bool1);
+        System.out.println("5 > 7 =" + bool2);
+    }
+    
+}
