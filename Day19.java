ccac1a5524d8eb423b702125dc00bbb1b69714f8
Jodim275 <myacoun447@gmail.com>
2024-09-20 22:10:59 +0800

    Create Day19.java
    Tipe data : String

---
+++ Day19.java
@@ -0,0 +1,10 @@
+package Coding100Hari;
+
+public class Day19 {
+    
+    public static void main(String[] args) {
+        String materi = "tipe data String";
+        System.out.println("Materi hari ini : " + materi);
+    }
+    
+}
