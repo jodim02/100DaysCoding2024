d8d16e6b5bdf70318868f9964095f81e8a3f31b1
Jodim275 <myacoun447@gmail.com>
2024-09-21 22:25:39 +0800

    Day20.java
    Tipe data reference: Byte

---
+++ Day20.java
@@ -0,0 +1,8 @@
+package Coding100Hari;
+
+public class Day20 {
+    public static void main(String[] args) {
+        Byte nilaiByte = 20;
+        System.out.println("Nilai Byte = " + nilaiByte );
+    }
+}
