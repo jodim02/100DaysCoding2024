a67c74f2b8563683d6b5b7b88d06b2de1998d92d
Jodim275 <myacoun447@gmail.com>
2024-09-22 23:07:51 +0800

    Day21.java
    Tipe data reference: Short

---
+++ Day21.java
@@ -0,0 +1,8 @@
+package Coding100Hari;
+public class Main {  
+    public static void main(String[] args) {
+        
+        Short hari = 21;
+        System.out.println("Tugas coding hari " + hari);
+    } 
+}
