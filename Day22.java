7b919f0cacc87dc679a1c9733a70e9331f65836c
Jodim275 <myacoun447@gmail.com>
2024-09-23 22:55:45 +0800

    Day22.java
    Tipe data reference: Integer

---
+++ Day22.java
@@ -0,0 +1,8 @@
+package Coding100Hari;
+public class Main {  
+    public static void main(String[] args) {
+        
+        Integer hari = 22;
+        System.out.println("Tugas Coding Hari ke "+ hari);
+    }   
+}
