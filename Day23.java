de83dfa8449de9f26dcab61e45deea7ab732a8a1
Jodim275 <myacoun447@gmail.com>
2024-09-24 21:54:31 +0800

    Day23.java
    Tipe data reference : Long

---
+++ Day23.java
@@ -0,0 +1,9 @@
+package Coding100Hari;
+public class Day23
+    public static void main(String[] args) {
+        
+        Long nilaiL = 24092024L; 
+        System.out.println("Nilai Long = " + nilaiL);
+
+    }
+}
