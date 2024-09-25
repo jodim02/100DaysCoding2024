67ce5953d73f6b60f2edb575db049b3b7cbc717f
Jodim275 <myacoun447@gmail.com>
2024-09-25 22:45:16 +0800

    Create Day24.java
    Tipe data reference: Float

---
+++ Day24.java
@@ -0,0 +1,10 @@
+package Coding100Hari;
+public class Main {   
+    public static void main(String[] args) {
+        
+        Float suhu = 36.5f;
+        System.out.println("Suhu di siamg hari " + suhu + "°C");
+      
+    }
+    
+}
