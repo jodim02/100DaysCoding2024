11f02c34e1d3291cd41be0386be00df19de68eab
Jodim275 <myacoun447@gmail.com>
2024-09-26 22:54:44 +0800

    Day25.java
    Tipe data reference: Double

---
+++ Day25.java
@@ -0,0 +1,11 @@
+package Coding100Hari;
+
+public class Day25 {
+    
+    public static void main(String[] args) {
+        
+        Double panjang = 26.9;
+        System.out.println("Nilai panjang : " + panjang);
+    }
+    
+}
