af7221ac90f47744099adb56d9fef8e3a7066783
Jodim275 <myacoun447@gmail.com>
2024-09-27 22:03:04 +0800

    Day26.java
    Tipe data reference: Character

---
+++ Day26.java
@@ -0,0 +1,11 @@
+package Coding100Hari;
+
+public class Day26 {
+    
+    public static void main(String[] args) {
+        
+        Character kelas = 'B';
+        System.out.println("Saya di kelas " + kelas);
+    }
+    
+}
