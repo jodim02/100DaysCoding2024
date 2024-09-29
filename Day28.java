792d0bf01d66837b51efbe384ce6848e1ef75f31
Jodim275 <myacoun447@gmail.com>
2024-09-29 22:31:55 +0800

    Day28.java
    Tipe data reference: Boolean

---
+++ Day28.java
@@ -0,0 +1,10 @@
+package Coding100Hari;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Boolean nilai = true;
+        System.out.println("Nilai 2 < 8 : " + nilai);
+    }
+    
+}
