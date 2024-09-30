5a97198664de6101b73da120e31de3a3082bbe54
Jodim275 <myacoun447@gmail.com>
2024-09-30 23:07:52 +0800

    Day29.java
    Tipe data reference: Biginteger

--- Day29.java
+++ Day29.java
@@ -1,7 +1,7 @@
 package Coding100Hari;
 import java.math.BigInteger;
 
-public class Main {
+public class Day29 {
     public static void main(String[] args) {
         BigInteger angka1 = new BigInteger("12345");
         BigInteger angka2 = new BigInteger("6789");
