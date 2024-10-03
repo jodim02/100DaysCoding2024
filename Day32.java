1bb7d4d88d60353efd81d45872cc2c909b5544f1
Jodim275 <myacoun447@gmail.com>
2024-10-03 21:35:45 +0800

    Day32.java
    Konversi tioe data primitif ke String

---
+++ Day32.java
@@ -0,0 +1,18 @@
+package Coding100Hari;
+
+public class Day32 {
+    
+    public static void main(String[] args) {
+        //Konversi int ke String
+        int a = 32;
+        String b = String.valueOf(a);
+        
+        //Konversi double ke String
+        double c = 32.0;
+        String d = String.valueOf(c);
+        
+        System.out.println("Konversi ke int = " + a);
+        System.out.println("Konversi ke doble = " + c);
+    }
+    
+}
