1c03fa49300a286f3c5ac6da40495c2a45fb25e6
Jodim275 <myacoun447@gmail.com>
2024-10-04 22:40:34 +0800

    Day33.java
    Type Casting

---
+++ Day33.java
@@ -0,0 +1,20 @@
+package Coding100Hari;
+
+public class Day33 {
+    
+    public static void main(String[] args) {
+        
+        //Widening Casting
+        int A = 33;
+        double B = A;
+        
+        //Narrowing Casting
+        double C = 33.0;
+        int D = (int) C;
+        
+        //Output
+        System.out.println("Nilai 1 : " + B);
+        System.out.println("Nilai 2 : " + D);
+    }
+    
+}
