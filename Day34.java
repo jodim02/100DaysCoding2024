012d3f7887055815da9571791a04c3bf5d3eca73
Jodim275 <myacoun447@gmail.com>
2024-10-05 22:26:17 +0800

    Day34.java
    Input data dari keyboard

---
+++ Day34.java
@@ -0,0 +1,15 @@
+package Coding100Hari;
+import java.util.Scanner;
+public class Day34 {
+    
+    public static void main(String[] args) {
+        Scanner Data = new Scanner(System.in);
+        System.out.print("Masukkan nama : ");
+        String nama = Data.nextLine();
+        System.out.print("Masukkan Umur : ");
+        int umur = Data.nextInt();
+        System.out.print("Masukkan Berat Badan : ");
+        double beratBadan = Data.nextDouble();
+    }
+    
+}
