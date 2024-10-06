bf15ae19574dc10016ae31f1bd3e32920de6b36d
Jodim275 <myacoun447@gmail.com>
2024-10-06 21:09:17 +0800

    Day35.java
    Operator aritmetika: Penjumlahan & Pengurangan

---
+++ Day35.java
@@ -0,0 +1,16 @@
+package Coding100Hari;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 5;
+    
+    //Penjumlahan & pengurangan
+        int tambah = a + b;
+        int kurang = a - b;
+        System.out.println("Nilai a + nilai b = " + tambah);
+        System.out.println("Nilai a - nilai b = " + kurang);
+    }
+    
+}
