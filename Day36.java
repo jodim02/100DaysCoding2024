1029c3153ba87589b0d08c7ceab4ff82b8e611c9
Jodim275 <myacoun447@gmail.com>
2024-10-07 22:20:44 +0800

    Day36.java
    Operator Aritmetika: Perkalian, Pembagian, dan Modulus

---
+++ Day36.java
@@ -0,0 +1,22 @@
+package Coding100Hari;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Deklarasi variabel:
+        int a = 30;
+        int b = 6;
+        int c = 4;
+        
+        //Operasi:
+        int kali = a * b;//Perkalian
+        int bagi = a / b;//Pembagian
+        int sisaBagi = a % c;//Modulus
+        
+        //Output
+        System.out.println("Hasil a x b = " + kali);
+        System.out.println("Hasil a / b = " + bagi);
+        System.out.println("Hasil a % c = " + sisaBagi);
+    }
+    
+}
