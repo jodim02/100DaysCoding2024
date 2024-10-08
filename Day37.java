13ac8a70fc31b4d67953d75039d0638736674077
Jodim275 <myacoun447@gmail.com>
2024-10-08 22:05:10 +0800

    Day37.java
    Operator Penugasan : Pengisian Nilai, Pengisian NIlai dan Penambahan, Pengisian Nilai dan Pengurangan

---
+++ Day37.java
@@ -0,0 +1,20 @@
+package Coding100Hari;
+
+public class Day37{
+
+    public static void main(String[] args) {
+        // Operator Penugasan
+        // Pengisian Nilai 
+        int x = 37;
+        System.out.println("Nilai x = " + x);
+        // Pengisian dan Penambahan 
+        int y = 30;
+        y += 7;
+        System.out.println("Penambahan nilai x = " + y);
+        // Pengisian dan Pengurangan
+        int z = 37;
+        z -= 7;
+        System.out.println("Pengurangan nilai z " + z);
+    }
+    
+}
