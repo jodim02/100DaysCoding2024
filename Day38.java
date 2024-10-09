aa9493bf9eb73561638e384f5c2e879f6a268cbc
Jodim275 <myacoun447@gmail.com>
2024-10-09 22:24:06 +0800

    Day38.java
    Operator Penugasan : Pengisian dan Perkalian, Pengisian dan Pembagian

---
+++ Day38.java
@@ -0,0 +1,16 @@
+package Coding100Hari;
+
+public class Day38{
+	public static void main(String[] args){
+
+		int x = 10;
+		// Pengisian dan Perkalian
+		x *= 5;
+		System.out.println("Hasil Perkalian : " + x);
+
+		// Pengisian dan Pembagian
+		x /= 2; 
+		System.out.println("Hasil Pembagian : " + x);
+	}
+
+}
