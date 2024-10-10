4cb75df6b57347283ec68fd38a744b3e12658f0c
Jodim275 <myacoun447@gmail.com>
2024-10-10 22:08:50 +0800

    Day39.java
    Operator penugasan: Pengisian dan Modulo

--- Day39.java
+++ Day39.java
@@ -4,10 +4,10 @@ public class Day39 {
     
     public static void main(String[] args) {
         //Deklarasi variabel
-        int nilai = 9;
+        int x = 9;
         //Pengisian dan Modulo
         nilai %= 2;
-        System.out.println("Sisa Bagi : " + nilai);
+        System.out.println("Sisa Bagi : " + x);
     }
     
 }
