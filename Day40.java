e296b10ee6f6bce449e1f5d888319e3e484e5232
Jodim275 <myacoun447@gmail.com>
2024-10-11 22:08:05 +0800

    Day40.java
    Operator perbandingan : Lebih besar dan lebih kecil

---
+++ Day40.java
@@ -0,0 +1,13 @@
+package Coding100Hari;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        System.out.println("x < y : " + (x<y));//Bernilai true
+        System.out.println("x > y : " + (x>y));//Bernilai false 
+      
+    }
+    
+}
