c01cd7b91582d169f2110315c9d91ef3218e9d6f
Jodim275 <myacoun447@gmail.com>
2024-10-14 21:38:59 +0800

    Day43.java
    Operator perbandingan: Lebih besar sama dengan dan lebih kecil sama dengan

---
+++ Day43.java
@@ -0,0 +1,15 @@
+package Coding100Hari;
+
+public class Day43 {
+    
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int x = 2, y = 3, z = 4;
+        // Perbandingan 
+        Boolean p = x >= y;
+        Boolean q = y <= z;
+        System.out.println("Nilai x >= nilai y : " + p);
+        System.out.println("Nilai y <= nilai z : " + q);
+    }
+    
+}
