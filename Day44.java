c2b866e37c2bc3e4d5d9f2fc27c12083d26b2c65
Jodim275 <myacoun447@gmail.com>
2024-10-15 21:01:47 +0800

    Day44.java
    Operator Logika AND

---
+++ Day44.java
@@ -0,0 +1,16 @@
+package Coding100Hari;
+public class Day44{
+  public static void main(String[] args){
+    //Deklarasi dan inisialisasi
+    int a = 4, b = 5, c = 6;
+          
+    //Operator logika &&
+    Boolean y = a < b && b > c;
+    Boolean z = a < b && b < c;
+          
+    //Output
+    System.out.println("Hasil dari a < b && b > c adalah : " + y);
+    System.out.println("Hasil dari a < b && b < c adalah : " + z);
+  }
+
+}
