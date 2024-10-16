268158af2291ab351d75d978f2938ea162d85cd2
Jodim275 <myacoun447@gmail.com>
2024-10-16 22:58:49 +0800

    Day45.java
    Operator logika : OR

---
+++ Day45.java
@@ -0,0 +1,13 @@
+package Coding100Hari;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 4, b = 5, c = 6;
+        Boolean x = a < b || b > c;
+        Boolean y = a > b || b > c;
+        System.out.println(x);
+        System.out.println(y);
+    }
+    
+}
