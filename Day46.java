bf5cc3baa753be57673b9e524e5d217cc87bfdd2
Jodim275 <myacoun447@gmail.com>
2024-10-17 23:24:40 +0800

    Day46.java
    Operator logika : Negasi / kebalikan

---
+++ Day46.java
@@ -0,0 +1,14 @@
+package Coding100Hari;
+
+public class Day46 {
+    
+    public static void main(String[] args) {
+        Boolean A = true;
+        System.out.println("Nilai A = " + A);
+        
+        //Negasi A
+        A = !A;
+        System.out.println("Negasi A = " + A);
+    }
+    
+}
