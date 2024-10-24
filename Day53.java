d8fa85948a6c6ee92f984ed12c78d194ea702eff
Jodim275 <myacoun447@gmail.com>
2024-10-24 22:56:20 +0800

    Day53.java
    Looping : for

---
+++ Day53.java
@@ -0,0 +1,9 @@
+package project100dayscoding;
+
+public class Day53 {
+    public static void main(String[] args) {
+        for(int a=0; a<10; a++){
+            System.out.println(a);
+        }
+    }
+}
