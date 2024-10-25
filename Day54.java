1c89fc6b14b69299d651851600377333ebc04423
Jodim275 <myacoun447@gmail.com>
2024-10-25 22:25:24 +0800

    Day54.java
    Looping : for increment

---
+++ Day54.java
@@ -0,0 +1,10 @@
+package project100dayscoding;
+
+public class Day54 {
+    public static void main(String[] args) {
+        // Looping for increment
+        for(int nilai=1; nilai<=5; nilai++){
+            System.out.println("Nilai : "+nilai);
+        }
+    }
+}
