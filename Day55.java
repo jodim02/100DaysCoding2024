d7ec054d71cad4e3cb41bf40c15a77506f723284
Jodim275 <myacoun447@gmail.com>
2024-10-26 21:34:40 +0800

    Day55.java
    Looping : for decrement

---
+++ Day55.java
@@ -0,0 +1,8 @@
+package project100dayscoding;
+public class Day55 { 
+    public static void main(String[] args) {
+        for(int b = 5; b > 0; b--){
+            System.out.println("Nilai b : "+b);
+        }
+    }    
+}
