4cbc5e043ae1d89c26dae81a1ed55645db4f642f
Jodim275 <myacoun447@gmail.com>
2024-10-28 22:31:16 +0800

    Day57.java
    Looping : while increment

---
+++ Day57.java
@@ -0,0 +1,12 @@
+package project100dayscoding;
+
+public class Day57 {
+    
+    public static void main(String[] args) {
+        int a = 1;
+        while(a <= 5){
+            System.out.println("Perulangan ke-" + a);
+            a++;
+        }
+    }    
+}
