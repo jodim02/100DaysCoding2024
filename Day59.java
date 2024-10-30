881634f93d7ca2a0121e6fe25733f58fd15df0c6
Jodim275 <myacoun447@gmail.com>
2024-10-30 22:53:58 +0800

    Day59.java
    Looping : for each

---
+++ Day59.java
@@ -0,0 +1,10 @@
+package project100dayscoding;
+
+public class Day59 {
+    public static void main(String[] args) {
+        char[] hurufVokal = {'a','i','u','e','o'};
+        for(char huruf : hurufVokal){
+            System.out.println(huruf);
+        }
+    }
+}
