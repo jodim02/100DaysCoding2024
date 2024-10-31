e4e4efe9272713bc28292c6d84078027f52438dd
Jodim275 <myacoun447@gmail.com>
2024-10-31 21:36:52 +0800

    Day60.java
    Looping : do while

---
+++ Day60.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+public class Main {
+    public static void main(String[] args) {
+        
+        int a = 1;
+        do {
+            System.out.println("Angka : " + a);
+            a++;
+        } while(a <= 5);
+    }
+}
