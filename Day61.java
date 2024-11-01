bfb9428ff8ddc12437405dd412a8c4b8aa6eb498
Jodim275 <myacoun447@gmail.com>
2024-11-01 20:51:13 +0800

    Day61.java
    Infinite loop

---
+++ Day61.java
@@ -0,0 +1,10 @@
+package project100dayscoding;
+
+public class Day61 {
+    
+    public static void main(String[] args) {
+        for ( int x = 1; x >= 0; x++){
+            System.out.println("Halo...");
+        }
+    }
+}
