700be530bfc4a2c86a8db6b567c4df1d61c4b07c
Jodim275 <myacoun447@gmail.com>
2024-11-02 22:00:22 +0800

    Day62.java
    Keyword break

---
+++ Day62.java
@@ -0,0 +1,13 @@
+package project100dayscoding;
+
+public class Day62 {
+    
+    public static void main(String[] args) {
+        for (int a = 1; a < 15; a++){
+            if (a == 11){
+                break;
+            }
+            System.out.print(a + " ");
+        }
+    }
+}
