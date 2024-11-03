22c5cbc5851e254a53051a51d83037bd4fb7bd29
Jodim275 <myacoun447@gmail.com>
2024-11-03 21:54:49 +0800

    Day63.java
    Keyword continue

---
+++ Day63.java
@@ -0,0 +1,15 @@
+package project100dayscoding;
+
+public class Day63 {
+    
+    public static void main(String[] args) {
+        for (int a = 1; a <= 5; a++){
+            if(a == 3){
+                continue;
+            }
+            System.out.print(a + " ");
+        }
+        
+    }
+    
+}
