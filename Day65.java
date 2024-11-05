ff39ae2a98dd9df96262b7706732b458518ac35f
Jodim275 <myacoun447@gmail.com>
2024-11-05 20:37:13 +0800

    Day65.java
    Nested loop

---
+++ Day65.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+public class Day65 {
+    public static void main(String[] args) {
+        for(int a = 1; a <= 5; a++){ //loop luar
+            for(int b = 1; b <=5; b++){ //loop dalam
+                System.out.print(b + " ");
+            }
+            System.out.println(" ");
+        }
+    }  
+}
