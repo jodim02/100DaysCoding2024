a32278c8d5ec2dab60ca8c8a83090c4a183cb4e3
Jodim275 <myacoun447@gmail.com>
2024-11-07 23:28:44 +0800

    Day67.java
    Method void

---
+++ Day67.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+public class Day67 {
+    public void belajar(){
+        System.out.println("Mari belajar java!");
+    }
+    public static void main(String[] args){
+        Day67 contoh = new Day67();
+        contoh.belajar();
+    }
+}
