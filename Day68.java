67e2a24451c6a3714fad0c8bff71598b898f5060
Jodim275 <myacoun447@gmail.com>
2024-11-08 23:49:38 +0800

    Day68.java
    Method non void

---
+++ Day68.java
@@ -0,0 +1,13 @@
+package project100dayascoding;
+
+public class Day68 {
+    public static int nilai(){
+        return 68;
+    }
+    
+    public static void main(String[] args) {
+        int angka = nilai();
+        System.out.println("Hasil : " + angka);
+    }
+    
+}
