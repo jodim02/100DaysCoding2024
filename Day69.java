ebb88adb5708e4e4d8e3ce782d4ed677547a14f7
Jodim275 <myacoun447@gmail.com>
2024-11-09 19:55:01 +0800

    Day69.java
    Evaluasi mentor

---
+++ Day69.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+public class Day69 {
+    
+    public static void main(String[] args) {
+        String[] Biodata = {"Nama\t: Jodim ","NIM\t: D0224312 ","Angkatan: 2024"};
+        for(String data : Biodata){
+            System.out.println(data);
+        }
+    }
+}
