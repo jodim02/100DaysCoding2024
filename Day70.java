d86e92ae9b0746069243ff3771f07bc15de423c5
Jodim275 <myacoun447@gmail.com>
2024-11-10 23:41:47 +0800

    Day70.java
    Method parameter

---
+++ Day70.java
@@ -0,0 +1,10 @@
+package project100dayscoding;
+
+public class Day70 {
+    public static void sapa(String nama){
+        System.out.println("Halo "+nama+", Selamat datang di pemrograman java");
+    }
+    public static void main(String[] args) {
+        sapa("Jodim");
+    }
+}
