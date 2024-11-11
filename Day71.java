f5f755287c8a703bdd151b798497c63d5bfbde3e
Jodim275 <myacoun447@gmail.com>
2024-11-11 23:21:44 +0800

    Day71.java
    Method static

---
+++ Day71.java
@@ -0,0 +1,11 @@
+package project100daysjava;
+
+public class Day71 {
+    public static void welcome(){
+        System.out.println("Selamat datang di program java!");
+    }
+    
+    public static void main(String[] args) {
+        Day71.welcome();
+    }
+}
