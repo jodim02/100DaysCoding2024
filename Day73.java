2a3ee4c5e54bf60fd4001577f788e3885481c156
Jodim275 <myacoun447@gmail.com>
2024-11-13 22:58:25 +0800

    Day73.java
    String method : charAt( )

---
+++ Day73.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String java = "CodingJava";
+        char chr = java.charAt(6);
+        System.out.println("Karakter ke-7 : "+chr);
+    }
+    
+}
