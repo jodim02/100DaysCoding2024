b4569c2f898fe65b3fa1e8317c2c2c85f7fbae39
Jodim275 <myacoun447@gmail.com>
2024-11-14 11:57:34 +0800

    Day74.java
    String method: length( )

---
+++ Day74.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+public class Day74 {
+    
+    public static void main(String[] args) {
+        String str = "Anak Informatika";
+        int jumlah = str.length();
+        System.out.println("Jumlah karakter : " + jumlah);
+    }
+    
+} 
