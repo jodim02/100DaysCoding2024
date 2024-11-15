a2030733e17db71a6bd045d8619299e206ddb43d
Jodim275 <myacoun447@gmail.com>
2024-11-15 15:41:18 +0800

    Day75.java
    String method: toLowerCase( )

---
+++ Day75.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+public class Day75 {
+    public static void main(String[] args) {
+        // String Method : toLowerCase()
+        
+        String text = "INFORMATIKA";
+        String lowerText = text.toLowerCase();
+        System.out.println(lowerText);
+    }
+}
