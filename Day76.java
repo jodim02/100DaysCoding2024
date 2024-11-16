50f1c6fc9f63e520a3ef7aa4d5e0d534564e1df0
Jodim275 <myacoun447@gmail.com>
2024-11-16 16:12:30 +0800

    Day76.java
    String method : toUpperCase( )

---
+++ Day76.java
@@ -0,0 +1,12 @@
+package project100dayscoding;
+
+public class Day76 {
+    
+    public static void main(String[] args) {
+        // String method : toUpperCase
+        String text = "tugas coding!";
+        String uppertext = text.toUpperCase();
+        System.out.println(uppertext);
+    }
+    
+}
