869fe281254bebbc6a0e6e6d6b6c0d7c4ced2cdf
Jodim275 <myacoun447@gmail.com>
2024-11-20 20:34:53 +0800

    Day80.java
    String method : trim( )

---
+++ Day80.java
@@ -0,0 +1,12 @@
+package project100dayscoding;
+
+public class Day80 {
+    
+    public static void main(String[] args) {
+        // String method trim()
+        String awal = "  Program Java  ";
+        String akhir= awal.trim();
+        System.out.println("Sebelum : '"+ awal + "'");
+        System.out.println("Sesudah : '"+ akhir + "'");
+    } 
+}
