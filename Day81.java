c48e7bafb027b9e73e283231b2f5fae26031fb93
Jodim275 <myacoun447@gmail.com>
2024-11-21 15:24:05 +0800

    Day81.java
    String method: valueOf()

---
+++ Day81.java
@@ -0,0 +1,10 @@
+package project100dayscoding;
+
+public class Day81 {
+    
+    public static void main(String[] args) {
+        char[] java = {'J','A','V','A'};
+        String program = String.valueOf(java);
+        System.out.println("Hasil : " + program);
+    }
+}
