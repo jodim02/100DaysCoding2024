c50455ae1bbb5d631179e17b73c561cdbd938925
Jodim275 <myacoun447@gmail.com>
2024-11-22 21:21:37 +0800

    Day82.java
    Method ceil, floor, dan round

--- Day82.java
+++ Day82.java
@@ -1,23 +1,10 @@
 package project100dayscoding;
-
-public class Main {
-    
+public class Day82 {    
     public static void main(String[] args) {
-        double nilai1 = 8.7;
-        double nilai2 = 8.2;
-        double nilai3 = 8.5;
-        
-        // Math.ceil
-        double hasil1 = Math.ceil(nilai1);
-        System.out.println("Math.ceil : " + hasil1);
-        
-        // Math.floor
-        double hasil2 = Math.floor(nilai2);
-        System.out.println("Math.floor : " + hasil2);
-        
-        // Math.round
-        long hasil3 = Math.round(nilai3);
-        System.out.println("Math.round : " + hasil3);
+        double nilai = 8.2;
+        System.out.println("Math.ceil  : " + Math.ceil(nilai));
+        System.out.println("Math.floor : " + Math.floor(nilai));
+        System.out.println("Math.round : " + Math.round(nilai));
     }
     
 }
