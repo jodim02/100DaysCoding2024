b7fda6dd1a54db9d59354b4cfe36a19197928524
Jodim275 <myacoun447@gmail.com>
2024-11-24 22:13:54 +0800

    Day83.java
    Membuat array satu dimensi

---
+++ Day83.java
@@ -0,0 +1,10 @@
+package project100dayscoding;
+
+public class Day83 {
+    
+    public static void main(String[] args) {
+        // Membuat array 1 dimensi
+        int[] nilai = new int[8];
+    }
+    
+}
