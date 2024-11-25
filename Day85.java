dfbcfdba613a1b499b039b42f21f6f69dc92a01b
Jodim275 <myacoun447@gmail.com>
2024-11-25 23:29:06 +0800

    Day85.java
    Mengisi elemen ke array satu dimensi

---
+++ Day85.java
@@ -0,0 +1,19 @@
+package project100dayscoding;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Mengisi elemen ke array 1 dimensi
+        // Cara1
+        int[] nilai = {1,2,3,4,5};
+        
+        // Cara2
+        int angka[] = new int[5];
+        angka[0] = 5;
+        angka[1] = 10;
+        angka[2] = 15;
+        angka[3] = 20;
+        angka[4] = 25;
+    }
+    
+}
