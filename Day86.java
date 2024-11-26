cf0ccfacb4101703a38bc9f789d7704b7443c085
Jodim275 <myacoun447@gmail.com>
2024-11-26 20:41:30 +0800

    Day86.java
    Mencetak elemen pada array 1 dimensi

---
+++ Day86.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+public class Day86 {
+    public static void main(String[] args) {
+        // Mencetak elemen pada array 1 dimensi
+        int[] nilai = {1,2,3,4,5};
+        for(int elemen : nilai){
+            System.out.println(elemen);
+        }
+    }
+}
